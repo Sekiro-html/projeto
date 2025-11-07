8cdc3339370d343f9c4195b4843443953f448582
Gabriel.SK <gabrielkonrad.rs@gmail.com>
2025-11-07 11:54:16 -0300

    main
    aaaaaah churrasco e fisica

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Ola Mundo")
+console.log("Ola Mundo");
+
+let churrasco = "sim";
+
+if ( churrasco == "sim") {
+    alert("vai ter churrasco!");
+} else {
+    alert("Então vamos marcar o churrasco!");
+}
